ef6705ed24ac0b8a9515ba31335e6f0bc2a91715
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2020-12-26 12:04:17 -0500

    Exposed environment variables

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/context_processors.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/context_processors.py
@@ -5,4 +5,8 @@ def settings_context(_request):
     """Settings available by default to the templates context."""
     # Note: we intentionally do NOT expose the entire settings
     # to prevent accidental leaking of sensitive information
-    return {"DEBUG": settings.DEBUG}
+    return {
+        "DEBUG": settings.DEBUG,
+        'ENVIRONMENT_NAME': settings.ENVIRONMENT_NAME,
+        'ENVIRONMENT_ADMIN_CSS': settings.ENVIRONMENT_ADMIN_CSS,
+    }
