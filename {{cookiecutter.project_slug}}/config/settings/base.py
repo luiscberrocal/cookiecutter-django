ebf35672c91548712744241cf695a06996a2d354
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-01-01 09:37:17 -0500

    Added core app to urls and settings

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -92,6 +92,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "{{ cookiecutter.project_slug }}.users.apps.UsersConfig",
+    '{{ cookiecutter.project_slug }}.core.apps.CoreConfig',
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
